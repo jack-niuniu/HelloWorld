3037f3e405a9dd4a66467943b1f26ee9275e49fe
jack-niuniu <hainiu312@gmail.com>
2023-07-03 11:30:24 +0800

    提交文件

---
+++ src/TestDoWhile.java
@@ -0,0 +1,11 @@
+	public class TestDoWhile{
+		public static void main(String[] args){
+			int x = 10;
+			
+			do{
+				System.out.println("xµÄÖµÊÇ" + x);
+				x++;
+				System.out.println("\n");
+			}while(x < 20);
+		}
+	}
---
+++ src/TestWhile.java
@@ -0,0 +1,11 @@
+
+public class TestWhile {
+	public static void main(String[] args){
+		int x = 10;
+		while(x < 20){
+			System.out.println("xµÄÖµÊÇ£»" + x);
+			x++;
+			System.out.println("\n");
+		}
+	}
+}
